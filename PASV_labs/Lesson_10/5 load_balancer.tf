a1f64e3865a4e652618a93fe042d1d2782775e15
SerhiiMyronets <hardmail88@gmail.com>
2024-08-08 13:57:30 -0700

    add lesson 15

--- PASV_labs/Lesson_10/5 load_balancer.tf
+++ PASV_labs/Lesson_10/5 load_balancer.tf
@@ -8,10 +8,12 @@ resource "aws_lb" "web_lb" {
 
 
 resource "aws_lb_target_group" "web_tg" {
-  name     = "web-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.defaul.id
+  name                 = "web-tg"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = data.aws_vpc.defaul.id
+  deregistration_delay = 30
+
 
   health_check {
     path                = "/"
